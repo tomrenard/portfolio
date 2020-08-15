cb86477ecca9741cea79164a0ef3cec38e86efee
Tom Renard <tom.renard@zoho.com>
2020-08-15 20:24:49 +0200

    new path correction

--- application.js
+++ application.js
@@ -1,7 +1,7 @@
 function hover(element) {
-  element.setAttribute('src', '/images/pe2.svg');
+  element.setAttribute('src', 'images/pe2.svg');
 }
 
 function unhover(element) {
-  element.setAttribute('src', '/images/per1.svg');
+  element.setAttribute('src', 'images/per1.svg)';
 }
