13887bc18c62d239d9de554f3e998f8e5c2a5d70
Tom Renard <tom.renard@zoho.com>
2020-09-14 23:39:39 +0200

    js improvement

---
+++ application.js
@@ -0,0 +1,11 @@
+function myGradientFunction() {
+   var element = document.body;
+   element.classList.toggle("gradient-mode");
+}
+
+function myWeirdFunction() {
+   var element = document.body;
+   // var elemento = document.getElementById("splash");
+   element.classList.toggle("weird-mode");
+   // elemento.classList.toggle("phone-mode");
+}
